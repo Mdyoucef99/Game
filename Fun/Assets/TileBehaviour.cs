9fd7b2578bb72165cd0907a8064503a51425d7c8
Youcef <mekki.d.youcef@outlook.fr4>
2020-01-27 20:42:40 -0500

    created tiles

---
+++ Fun/Assets/TileBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TileBehaviour : MonoBehaviour
+{
+    public Text scoretxt;
+    public GameObject tile;
+    public  int scoreint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+
+
+    public void Incrementor()
+    {
+        
+        Debug.Log("pressed!!");
+        Destroy(tile);
+      
+
+
+
+    }
+
+}
